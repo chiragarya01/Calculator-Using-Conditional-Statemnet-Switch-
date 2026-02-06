9ce881fd72ea62dbb89d102c2c08a5a62ae5fe69
Chirag Arya <chiragarya024@gmail.com>
2026-02-06 12:48:25 +0530

    Calculator Using Switch Statement In Java

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+/* 
+public class Calculate {
+    public static void main(String args[]) {
+
+        int a = 34;
+        int b = 34;
+
+        char perform = '+';
+
+        switch (perform) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+        }
+    }
+}
+ */
+
+import java.util.*;
+
+public class Calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char Calculate = sc.next().charAt(0);
+
+        switch (Calculate) {
+            case '+':
+                System.out.println("Your Sum Is After Adding " + a + b);
+                break;
+
+            case '_':
+                System.out.println("Your Sum Is After Substraction " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Your Sum Is After Multiplication " + (a * b));
+                break;
+
+            case '/':
+                System.out.println("Your Sum IS After Division " + (a / b));
+                break;
+
+            case '%':
+                System.out.println("Your Sum IS After Modulo " + (a % b));
+                break;
+
+            default:
+                System.out.println("Wroung! Operation Perform  Not in System ! ");
+        }
+    }
+}
